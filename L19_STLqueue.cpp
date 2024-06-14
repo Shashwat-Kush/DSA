9eaa242cbfcda6f1b9e880729b8c4bee96f4bd9f
Shashwat-Kush <shashwatkush002@kgpian.iitkgp.ac.in>
2024-06-14 23:31:25 +0530

    .

--- L19_STLqueue.cpp
+++ L19_STLqueue.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-#include <math.h>
+#include <queue>
 using namespace std;
+//Queue is FIFO
+//We can only insert the elements from the back of the queue
+//We can only delete the elements from the front of the queue
+//We can't insert and delete the elements from the middle of the queue
+//We can't access the elements of the queue directly
+//We can only access the elements by using the iterators
 
 int main(){
-    
+    queue<string> q;
+    q.push("Apple");
+    q.push("Mango");
+    q.push("Guava");
+    cout<<"Front element is "<<q.front()<<endl;
+    cout<<"Back element is "<<q.back()<<endl;
+    cout<<"Size of the queue is "<<q.size()<<endl;
+    q.pop();
+    cout<<"Front element is "<<q.front()<<endl;
+    cout<<"Size of the queue is "<<q.size()<<endl;
 }
