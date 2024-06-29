eaca0fcdb5d7a98ad45f5d5e1b5751f11b82e92c
Shashwat-Kush <shashwatkush002@kgpian.iitkgp.ac.in>
2024-06-29 16:28:27 +0530

    .

---
+++ L23_2DArray/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+// Visualize toh 2D array ho raha but save toh Linear array ki tarah ho raha tha
+// row then column
+// 
+int main(){
+    int arr[3][4];
+    for (int i=0;i<3;i++){
+        for (int j=0;j<4;j++){
+            cin >> ar[i][j];
+        }
+    }
+    
+    for (int i=0;i<3;i++){
+        for (int j=0;j<4;j++){
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+}
